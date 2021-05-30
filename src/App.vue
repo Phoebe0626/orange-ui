37e397668fdad182f502da39636b9727ccc98e88
phoebe <506014196@qq.com>
2021-05-30 17:49:49 +0800

    给button添加ghost和round属性

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,24 @@
       <o-button type="success">按钮</o-button>
       <o-button type="info">按钮</o-button>
       <o-button type="warning">按钮</o-button>
-      <o-button type="error">取消</o-button>
-      <o-button>取消</o-button>
+      <o-button type="error">按钮</o-button>
+      <o-button>按钮</o-button>
+    </div>
+    <div class="row">
+      <o-button ghost type="primary">按钮</o-button>
+      <o-button ghost type="success">按钮</o-button>
+      <o-button ghost type="info">按钮</o-button>
+      <o-button ghost type="warning">按钮</o-button>
+      <o-button ghost type="error">按钮</o-button>
+      <o-button ghost>按钮</o-button>
+    </div>
+    <div class="row">
+      <o-button round ghost type="primary">按钮</o-button>
+      <o-button round ghost type="success">按钮</o-button>
+      <o-button round ghost type="info">按钮</o-button>
+      <o-button round ghost type="warning">按钮</o-button>
+      <o-button round ghost type="error">按钮</o-button>
+      <o-button round ghost>按钮</o-button>
     </div>
   </div>
 </template>
@@ -19,6 +35,8 @@ export default {
 
 <style lang="scss">
 .row {
+  padding: 10px;
+  background-color: #bec8c8;
   margin-bottom: 20px;
   .o-button {
     margin-right: 20px;
--- src/components/button.vue
+++ src/components/button.vue
@@ -1,5 +1,11 @@
 <template>
-  <button class="o-button" :class="[`o-button--${type}`]">
+  <button
+    class="o-button"
+    :class="[`o-button--${type}`,
+      {'is-ghost': ghost},
+      {'is-round': round}
+    ]"
+  >
     <span><slot></slot></span>
   </button>
 </template>
@@ -11,7 +17,18 @@ export default {
     type: {
       type: String,
       default: 'default'
+    },
+    ghost: {
+      type: Boolean,
+      default: false
+    },
+    round: {
+      type: Boolean,
+      default: false
     }
+  },
+  created () {
+    console.log(this.ghost)
   }
 }
 </script>
@@ -29,7 +46,7 @@ export default {
   box-sizing: border-box;
   outline: none;
   // margin: 0;
-  transition: .1s;
+  transition: .2s;
   font-weight: 500;
   user-select: none;
   padding: 12px 20px;
@@ -101,5 +118,73 @@ export default {
     border-color: #f16643;
     color: #fff;
   }
+}
+// 幽灵按钮：将其他按钮的内容反色，背景变为透明，常用在有色背景上
+.o-button.is-ghost {
+  color: #fff;
+  background-color: transparent;
+  &:hover,
+  &:focus {
+    background-color: transparent;
+    border-color: #409eff;
+    color: #409eff;
+  }
+}
+
+.o-button--primary.is-ghost {
+  color: #2d8cf0;
+  &:hover,
+  &:focus {
+    border-color: inherit;
+    background-color: rgba(255, 255, 255, .4);
+  }
+}
+.o-button--success.is-ghost {
+  color: #19be6b;
+  &:hover,
+  &:focus {
+    color: #19be6b;
+    border-color: #19be6b;
+    background-color: rgba(255, 255, 255, .4);
+  }
+}
+.o-button--info.is-ghost {
+  color: #2db7f5;
+  &:hover,
+  &:focus {
+    color: #2db7f5;
+    border-color: #2db7f5;
+    background-color: rgba(255, 255, 255, .4);
+  }
+}
+.o-button--warning.is-ghost {
+  color: #ff9900;
+  &:hover,
+  &:focus {
+    color: #ff9900;
+    border-color: #ff9900;
+    background-color: rgba(255, 255, 255, .4);
+  }
+}
+.o-button--error.is-ghost {
+  color: #ed4014;
+  &:hover,
+  &:focus {
+    color: #ed4014;
+    border-color: #ed4014;
+    background-color: rgba(255, 255, 255, .4);
+  }
+}
+
+// 圆角按钮
+.o-button.is-round {
+  border-radius: 20px;
+  padding: 12px 23px;
+}
+
+// 圆形按钮
+.o-button.is-circle {
+  border-radius: 50%;
+  padding: 12px;
 }
 </style>
