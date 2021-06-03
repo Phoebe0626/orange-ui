b0ec742a27bacdbde867816caddb38760f556596
phoebe <506014196@qq.com>
2021-06-03 22:24:53 +0800

    tabs组件添加一个滑动的line

--- src/components/tabs.vue
+++ src/components/tabs.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="o-tabs">
-    <div class="o-tabs__nav">
-      <div class="o-tabs__active-bar"></div>
+    <div class="o-tabs__nav" ref="nav">
+      <div
+        class="o-tabs__active-line"
+        :style="lineStyle"
+      ></div>
       <div
         class="o-tabs__item"
         v-for="(tab, index) in tabs"
@@ -32,6 +35,9 @@ export default {
   },
   data () {
     return {
+      barOffset: 0,
+      barWidth: 0,
+      lineStyle: {},
       tabs: []
     }
   },
@@ -39,6 +45,33 @@ export default {
     this.tabs = this.getTabs()
   },
   methods: {
+    setLine (name) {
+      const index = this.tabs.findIndex(item => item.name === name)
+      const prevTabs = this.$refs.nav.querySelectorAll('.o-tabs__item')
+      if (index === 0 || index === prevTabs.length - 1) {
+        this.barWidth = prevTabs[index].offsetWidth - 20
+      } else {
+        this.barWidth = prevTabs[index].offsetWidth - 40
+      }
+
+      if (index > 0) {
+        let offset = 0
+        for (let i = 0; i < index; i++) {
+          console.log(index)
+          offset += prevTabs[i].offsetWidth
+        }
+        this.barOffset = offset + 20
+      } else {
+        this.barOffset = 0
+      }
+      this.lineStyle = {
+        width: this.barWidth + 'px',
+        transform: `translateX(${this.barOffset}px)`
+      }
+    },
+    getTabIndex (name) {
+      return this.tabs.findIndex(item => item.name === name)
+    },
     handleClick (name) {
       this.$emit('tab-click', name)
     },
@@ -53,6 +86,11 @@ export default {
       })
       return TabPanes
     }
+  },
+  watch: {
+    value (newVal) {
+      this.setLine(newVal)
+    }
   }
 }
 </script>
@@ -62,8 +100,21 @@ export default {
   box-sizing: border-box;
   position: relative;
   color: #515a6e;
+  overflow: hidden;
   .o-tabs__nav {
+    position: relative;
     margin-bottom: 15px;
+    overflow: hidden;
+    .o-tabs__active-line {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      height: 2px;
+      background-color: #2d8cf0;
+      transition: transform .3s cubic-bezier(.645,.045,.355,1);
+      list-style: none;
+      z-index: 2;
+    }
     .o-tabs__item {
       padding: 0 20px;
       height: 40px;
@@ -75,6 +126,16 @@ export default {
       font-weight: 500;
       position: relative;
       cursor: pointer;
+      z-index: 1;
+      &:nth-child(2) {
+        padding-left: 0;
+      }
+      &:last-child {
+        padding-right: 0;
+      }
+      &:hover {
+        color: #2d8cf0;
+      }
     }
     .o-tabs__item.is-active {
       color: #2d8cf0;
